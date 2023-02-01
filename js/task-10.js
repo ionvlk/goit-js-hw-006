b93a70335b934df3058a6dd18297e8d527e8f277
ionvlk <ionvasilache9001@gmail.com>
2023-02-01 22:18:01 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,31 +1,31 @@
-const boxesContainer = document.querySelector("#boxes");
-const createBtn = document.querySelector('[data-create]');
-const destroyBtn = document.querySelector('[data-destroy]');
-const input = document.querySelector('input[type="number"]');
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 
-createBtn.addEventListener('click', createBoxes);
-destroyBtn.addEventListener('click', destroyBoxes);
-
-function getRandomHexColor() {
-  return `#${(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
-
-function createBoxes() {
-  const count = input.value;
-  let width = 30;
-  let height = 30;
-
-  for (let i = 0; i < count; i++) {
-    const box = document.createElement('div');
-    box.style.width = `${width}px`;
-    box.style.height = `${height}px`;
+const createBoxes = (amount) => {
+  const boxes = document.querySelector("#boxes");
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(box);
-    width += 10;
-    height += 10;
+    boxes.appendChild(box);
+    size += 10;
   }
-}
+};
 
-function destroyBoxes() {
-  boxesContainer.innerHTML = '';
-}
+const destroyBoxes = () => {
+  document.querySelector("#boxes").innerHTML = "";
+};
+
+const controls = document.querySelector("#controls");
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const input = controls.querySelector("input");
+
+createButton.addEventListener("click", () => {
+  createBoxes(Number(input.value));
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
